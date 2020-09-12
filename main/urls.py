2391c3d5b104547d771f9a1da8a1a96b8050373d
diego-arc3 <63832928+diego-arc3@users.noreply.github.com>
2020-09-12 16:12:14 -0500

    views para producto

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('productos', views.ProductListView.as_view(), name='product-list'),
+    path('productos/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from .models import *
 
 def home(request):
     return HttpResponse("Hola Mundo. Te encuentras en la página de inicio de Linio Express")
 
 
 # Create your views here.
+
+class ProductListView(ListView):
+    model = Producto
+    
+class ProductDetailView(DetailView):
+    model = Producto
